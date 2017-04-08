76d40532e93c367a1fbd05bb2cef706d589c51de
kunoh <kunoh@live.dk>
2017-04-08 14:38:44 +0200

    Tilføjet egenskaber til player

--- src/awesomegame/Player.java
+++ src/awesomegame/Player.java
@@ -10,5 +10,26 @@ package awesomegame;
  * @author Kunoh
  */
 public class Player {
+    String firstname;
+    String lastname;
+    String name;
+    String nationality;
+    int age = 17;
+    int passing = 3;
+    int finishing = 3;
+    int heading = 3;
+    int technique = 3;
+    int dribbling = 3;
+    int tackling = 3;
+    int concentration = 3;
+    int positioning = 3;
+    int penalty = 3;
+    int freekick = 3;
+    int pace = 3;
+    int stamina = 3;
+    int vision = 3;
+    int crossing = 3;
+    int jumping = 3;
+    
     
 }
